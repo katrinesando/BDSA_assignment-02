5640434c5e6febd988d8985579b0410b02835f28
katrinesando <kmsa@itu.dk>
2022-09-21 17:43:36 +0200

    Extentension and linq methods and testing

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -5,6 +5,73 @@ public class ExtensionsTests
     [Fact]
     public void Test1()
     {
-
+        // Given
+        IEnumerable<int>[]xs = {new[]{1,2},new[]{3,4,5},new[]{6,7,8,9}};
+        
+        // When
+        var result = Extensions.Flatten<int>(xs);
+        // Then
+        Assert.Equal(new[]{1,2,3,4,5,6,7,8,9},result);
+        
+    }
+    [Fact]
+    public void Test2()
+    {
+        // Given
+        int[] ys= {1,2,7,8,49};
+        
+        // When
+        var result = ys.Where(x => x % 7 == 0 && x>42);
+        
+        //
+        result.Should().BeEquivalentTo(new[] { 49 });
+    }
+    [Fact]
+    public void Test3()
+    {
+        // Given
+        int[] ys= {2000,2004,1900};
+        
+        // When
+        var result = ys.Where(x => (x % 4 == 0 && x % 100 != 0) ||(x%400==0));
+        
+        //
+        result.Should().BeEquivalentTo(new[] {2000,2004});
+    }
+    [Fact]
+    public void HTTPS_IsSecure()
+    {
+        // Given
+        Uri website = new Uri("https://github.com/itu-bdsa/lecture-notes/blob/main/sessions/csharp_01/slides.md");
+        
+        // When
+        var result = Extensions.isSecure(website);
+        
+        //
+        result.Should().BeTrue();
+    }
+    [Fact]
+    public void WordCount_should_be_five()
+    {
+        // Given
+        string text = "this is a string wuuhuu";
+        
+        // When
+        var result = Extensions.WordCountOnlyLetters(text);
+        
+        //
+        result.Should().Be(5);
+    }
+    [Fact]
+    public void WordCount_With_Letters()
+    {
+        // Given
+        string text = "this is a 1 string with symbols &3#";
+        
+        // When
+        var result = Extensions.WordCountOnlyLetters(text);
+        
+        //
+        result.Should().Be(6);
     }
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -1,5 +1,87 @@
+using System.Xml;
+
 namespace Assignment2;
+using System.Linq;
+using System.Collections.Generic;
 
 public class Queries
 {
+    //1
+    public static IEnumerable<string> ByRowling(IEnumerable<Wizard> wizards)
+    {
+        var fromCreator = from h in wizards
+            where h.Creator.Equals("J.K. Rowling") 
+            select h.Name;
+
+        return fromCreator.ToList();
+    }  
+    public static IEnumerable<string> ByRowlingExtension(IEnumerable<Wizard> wizards)
+    {
+        var fromCreator = wizards.Where(h => h.Creator.Equals("J.K. Rowling")).Select(h => h.Name);
+
+        return fromCreator.ToList();
+    }
+    //2
+    public  static int? FindSithLord(IEnumerable<Wizard>wizards)
+    {
+        return (from w in wizards
+            where w.Name.Contains("Darth")
+            select w.Year).First();
+    }
+    public  static int? FindSithLordExtension(IEnumerable<Wizard>wizards)
+    {
+        return (wizards.Where(w => w.Name.Contains("Darth")).Select(w=>w.Year)).First() ;
+    }
+    //3
+    public static IEnumerable<(string,int?)> FromHarryPotter(IEnumerable<Wizard> wizards)
+    {
+        return from w in wizards
+            where w.Medium.Contains("Harry Potter")
+            select(w.Name,w.Year);
+    }  
+    public static IEnumerable<(string name,int? year)> FromHarryPotterExtension(IEnumerable<Wizard> wizards)
+    {
+        var fromCreator = wizards
+            .Where(h => h.Medium.Equals("Harry Potter"))
+            .Select(h =>new
+            { h.Name,h.Year})
+            .Distinct()
+            .AsEnumerable()
+            .Select(h=> (h.Name,h.Year));
+
+        return fromCreator;
+    }
+    
+    //4
+    public static IEnumerable<string> ReverseGrouped(IEnumerable<Wizard> wizards)
+    {
+        var reverseGrouped = (from w in wizards
+            orderby w.Creator descending, w.Name
+            group w by w.Creator
+            into grouping
+            let wnames = from h2 in grouping
+                select h2.Name
+            select wnames) ;
+        
+        foreach (var wizard in reverseGrouped)
+        {
+            foreach (var name in wizard)
+            {
+                yield return name;
+            }
+        }
+    }
+    public static IEnumerable<string> ReverseGroupedExtension(IEnumerable<Wizard> wizards)
+    {
+        var reverseGrouped =
+            wizards.OrderByDescending(w => w.Creator).ThenBy(w => w.Name).GroupBy(w => w.Creator)
+                .Select(grouping => grouping.Select(h2=>h2.Name));
+        foreach (var wizard in reverseGrouped)
+        {
+            foreach (var name in wizard)
+            {
+                yield return name;
+            }
+        }
+    }
 }
